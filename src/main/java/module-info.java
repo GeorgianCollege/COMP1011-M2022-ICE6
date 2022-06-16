1abebd22da56ba842241da28dad48347eab2fe3b
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-06-16 19:08:29 -0400

    added UI Elements and related controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ module ca.georgiancollege.comp1011m2022ice6 {
     requires javafx.fxml;
     requires java.sql;
 
-
     opens ca.georgiancollege.comp1011m2022ice6 to javafx.fxml;
     exports ca.georgiancollege.comp1011m2022ice6;
 }
